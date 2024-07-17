9fd0c785a66edcdddd00cab86e02317a44f69b49
Ayka_667 <164785580+ayka667@users.noreply.github.com>
2024-07-17 11:09:27 +0200

    Add files via upload

---
+++ www.andapirate.com/_next/static/chunks/pages/wtf-a53d2b07c8306f5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[122],{8004:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/wtf",function(){return u(1122)}])}},function(n){n.O(0,[66,888,774,179],function(){return n(n.s=8004)}),_N_E=n.O()}]);
